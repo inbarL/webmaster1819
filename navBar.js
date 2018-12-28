b781428149530a51f5b553bf4084165c796b5b65
inbarmada <32581040+inbarmada@users.noreply.github.com>
2018-12-27 19:05:22 -0600

    Move navBar to js file

---
+++ navBar.js
@@ -0,0 +1,8 @@
+document.write("<nav>");
+document.write("    <ul>");
+document.write("      <li><a href=\"index.html\" class=\"homeLink\"><b>Home<\/b><\/a><\/li>");
+document.write("      <li><a href=\"about.html\">About<\/a><\/li>");
+document.write("      <li><a href=\"photos.html\">Photos<\/a><\/li>");
+document.write("      <li><a href=\"music.html\">Music<\/a><\/li>");
+document.write("    <\/ul>");
+document.write("  <\/nav>");
