4aebabbc230c3e0b97f08b8fce7c36990e8b58b3
inbarmada <32581040+inbarmada@users.noreply.github.com>
2018-12-28 09:23:24 -0600

    Fix navBar.js

--- navBar.js
+++ navBar.js
@@ -1,8 +1,7 @@
-document.write("<nav>");
-document.write("    <ul>");
-document.write("      <li><a href=\"index.html\" class=\"homeLink\"><b>Home<\/b><\/a><\/li>");
-document.write("      <li><a href=\"about.html\">About<\/a><\/li>");
-document.write("      <li><a href=\"photos.html\">Photos<\/a><\/li>");
-document.write("      <li><a href=\"music.html\">Music<\/a><\/li>");
-document.write("    <\/ul>");
-document.write("  <\/nav>");
+
+document.getElementsByTagName("NAV")[0].innerHTML = "    <ul>" +
+"      <li><a href=\"index.html\" class=\"homeLink\"><b>Home<\/b><\/a><\/li>" +
+"      <li><a href=\"about.html\">About<\/a><\/li>" +
+"      <li><a href=\"photos.html\">Photos<\/a><\/li>" +
+"      <li><a href=\"music.html\">Music<\/a><\/li>" +
+"    <\/ul>";
