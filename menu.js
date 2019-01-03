88ae09c9ba8cc7025cd1aab01f7449e19c0fa703
inbarmada <32581040+inbarmada@users.noreply.github.com>
2019-01-02 19:06:49 -0600

    Make menu become fixed when scrolling up - semi-working

--- menu.js
+++ menu.js
@@ -3,3 +3,22 @@ document.getElementsByTagName("NAV")[0].innerHTML = "    <ul>" +
 "      <li><a href=\"CTE.html\" class=\"homeLink\">CTE<\/a><\/li>" +
 "      <li><a href=\"TSA.html\">TSA<\/a><\/li>" +
 "    <\/ul>";
+
+console.log("hi");
+
+window.onscroll = function(e){
+  var direction = this.oldScroll > this.scrollY;
+  // console.log(direction);
+  this.oldScroll = this.scrollY;
+  show(direction);
+}
+
+
+var show = function(a){
+  // console.log("in show, a = " + a);
+  if(a == true){
+    document.getElementsByTagName("NAV")[0].style.position = 'fixed';
+  }else{
+    document.getElementsByTagName("NAV")[0].style.position = 'relative';
+  }
+}
