0a942b9bb4f2a69367404d98e61834a689bf61c1
Yuvali Leibovich <yuvali.leibovich475@student.mckinneyisd.net>
2019-01-11 14:56:41 -0600

    white house phone number changed

--- schoolFooter.js
+++ schoolFooter.js
@@ -1,2 +1,2 @@
-document.getElementsByTagName("FOOTER")[0].innerHTML = "Call Us: (469)302-3400 <br>"+
+document.getElementsByTagName("FOOTER")[0].innerHTML = "Call Us: (469) 302-3400 <br>"+
 "600 N. Lake Forest Dr., McKinney, TX 75071<br><a href=Band/index.html>Website</a>";
